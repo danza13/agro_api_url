f44df7f198a3c89e95fc19548fbb2eb5ce52a04a
danza13 <136966054+danza13@users.noreply.github.com>
2025-02-16 17:56:15 +0200

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -47,4 +47,5 @@ def get_data():
     return jsonify({"status": "ok"}), 200
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
